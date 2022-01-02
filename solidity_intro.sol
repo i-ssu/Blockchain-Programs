2ed2cd53a987226c6a4c5a2b1193dda0ec1ae5dc
i-ssu <68648156+i-ssu@users.noreply.github.com>
2022-01-03 03:03:10 +0530

    Update solidity_intro.sol

--- solidity_intro.sol
+++ solidity_intro.sol
@@ -1,1 +1,15 @@
+#Simple Smart Contract
 
+pragma solidity ^0.4.7;
+contract Simple_Contract 
+{
+	uint var;
+	function set(uint x) public 
+	{ 
+		var=x;
+	}
+	function get(uint x) public
+	{
+		return x;
+	}
+}
